88cbd498504f6af20fdccc71c09d174e2cc1c738
Maddoxrs33 <buddyray04@gmail.com>
2026-03-18 14:33:36 -0600

    Create multiply.py
    Wrote a function that accepts two numbers and multiplies

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Maddox Swift
+#Section 3
+#3/16/26
+# Description: Accept two numbers and return the product
+
+def multiplyTwoNumbers(iNum1, iNum2):
+    #Multiply and return the result
+    return iNum1*iNum2
